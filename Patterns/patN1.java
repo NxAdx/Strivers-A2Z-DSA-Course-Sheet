4859705af5674b815ecedc7d976b5e3c2005a385
“[NxAdx]” <[lokhandeaadarsh28@gmail.com]>
2024-01-22 17:54:13 +0530

    first commit

---
+++ Patterns/patN1.java
@@ -0,0 +1,10 @@
+class patN1{
+	public static void main(String ar[]){
+	   for(int i=0;i<5;i++){
+              for(int j=1;j<5;j++){
+                 System.out.println("*");
+      }   
+       System.out.println();
+    }
+  }
+} 
